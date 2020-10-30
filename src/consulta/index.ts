925eacef371d285dc07e137a07d896a6e2c6cc79
Ruddá Beltrão <beltrao.rudah@gmail.com>
2020-10-29 23:46:31 -0400

    feature: add nota fiscal eletronica UF Amazonas

--- src/consulta/index.ts
+++ src/consulta/index.ts
@@ -2,5 +2,6 @@ import rs from './rs';
 import mg from './mg';
 import ce from './ce';
 import rn from './rn';
+import am from './am';
 
-export default { rs, mg, ce, rn };
+export default { rs, mg, ce, rn, am };
